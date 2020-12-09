8666e279ffd55ac4839b24c3942a507b5622ffd4
Stacey McKnight <mcknight.stm@gmail.com>
2020-12-09 09:18:52 -0600

    commit tree a33d65f546b75f34e852405589ec93631f115edf

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+
+katz_deli = []
+
+def line(katz_deli)
+    if katz_deli.size == 0
+        puts "The line is currently empty."
+    else 
+        line_list = "The line is currently:"
+        katz_deli.each.with_index(1) do | name, index |
+            line_list << " #{index}. #{name}"
+        end
+        puts line_list
+    end
+end
+
+def take_a_number(katz_deli, name)
+    katz_deli << name
+    puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+end
+
+def now_serving(katz_deli)
+    if katz_deli.size == 0
+        puts "There is nobody waiting to be served!"
+    else 
+        puts "Currently serving #{katz_deli[0]}."
+        katz_deli.shift
+    end
+end
